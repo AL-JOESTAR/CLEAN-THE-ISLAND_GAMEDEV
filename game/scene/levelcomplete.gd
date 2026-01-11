e4e94b6d3431800edecd52bdeba89becd7c0a6e9
AL JOESTAR <156530138+AL-JOESTAR@users.noreply.github.com>
2026-01-11 22:49:59 +0700

    Delete game directory

--- game/scene/levelcomplete.gd
+++
@@ -1,8 +0,0 @@
-extends Control
-
-func _on_lanjut_pressed() -> void:
-	get_tree().change_scene_to_file("res://level/level2/trashhunt.tscn")
-
-
-func _on_menu_pressed() -> void:
-	get_tree().change_scene_to_file("res://scene/main_menu.tscn")
